deb8389fae6945d4453bc6b70aeb07d507f12a19
hirakiuc <hirakiuc@gmail.com>
2018-03-27 12:17:21 +0900

    :shirt: fix lint errors.

--- lib/feedes/hatena.rb
+++ lib/feedes/hatena.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_relative './fetcher.rb'
-require_relative './parser/hatena_hotentry_parser.rb'
+require_relative './parser/hatena/hotentry_parser.rb'
 
 module Feedes
   module Hatena
@@ -13,15 +13,13 @@ module Feedes
       # @option options [Array] :headers Array of request headers.
       # @option options [Array] :content_types Array of acceptable content-type strings.
       def hotentry(theday, options = {})
-        # TODO validate theday format YYYYMMDD
+        # TODO: validate theday format YYYYMMDD
         url = "http://b.hatena.ne.jp/hotentry/#{theday}?layout=list"
 
-        opts = options.merge({
-          content_types: ['text/html']
-        })
+        opts = options.merge(content_types: ['text/html'])
         res = Fetcher.new(opts).fetch(url)
 
-        parser = Parser::HatenaHotentryParser.new(theday)
+        parser = Parser::Hatena::HotentryParser.new(theday)
 
         parser.parse(res.body)
       end
--- lib/feedes/model/hatena/hotentry.rb
+++ lib/feedes/model/hatena/hotentry.rb
@@ -4,7 +4,7 @@ module Feedes
   module Model
     module Hatena
       class Hotentry
-        attr_reader :title, :description, :date, :url, :attrs
+        attr_reader :title, :description, :date, :url, :theday, :attrs
         attr_reader :hatebu_count, :rank, :category
 
         def initialize(theday, attrs)
@@ -12,6 +12,7 @@ module Feedes
           @description = attrs[:desc]
           @date = attrs[:dc_date]
           @url = attrs[:link]
+          @theday = theday
 
           @hatebu_count = attrs[:hatebu_count]
           @rank = attrs[:rank]
